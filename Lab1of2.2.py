2438fdbc409127a4582a1aa9a79911a35b9a22d9
ibrogenius <ibrogenius5165@gmail.com>
2020-07-31 16:39:58 +0100

    Adding Lab1of2.2

---
+++ Lab1of2.2.py
@@ -0,0 +1,11 @@
+population_density = (10/1)
+test1 = population_density
+expected_result = 10
+print("expected result: {}, actual result: {}\n".format(expected_result, test1))
+
+population_density = (864816/121.4)
+test2 = population_density
+expected_result2 = 7123.6902801
+print("expected result: {}, actual result: {}".format(expected_result2, test2))
+
+# i used the normal formula for calculating density which is Density=mass/volume
